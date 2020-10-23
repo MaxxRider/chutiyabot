66c70902540dd5230f81a2e1af40bdcf359bd6ad
RJ HANIF AHMED <66148227+MaxxRider@users.noreply.github.com>
2020-10-23 08:22:27 +0530

    Create config.py

---
+++ robote/config.py
@@ -0,0 +1,13 @@
+from robote.dev_config import Config
+
+class Config(Config):
+ TEGE_TOKEN= ""
+ TEGE_API_ID =
+ TEGE_API_HASH = ""
+ TEGE_OWNER =
+ AUTH_CHANNEL = []
+ LEECH_COMMAND = ""
+ CANCEL_COMMAND_G = ""
+ DESTINATION_FOLDER = ""
+ UPLOADER_CONFIG = """"""
+ #dont remove """ from both side of the UPLOADER_CONFIG
